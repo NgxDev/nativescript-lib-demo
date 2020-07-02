00fe6c4fd08a7234f9fc1cf2f912f15ff1b1b82c
Marian Stoica <marian.stoica@web-prime.ro>
2020-07-02 08:06:55 +0300

    pretty import path (not necessary, but nice to have)

--- projects/nativescript-lib-demo/src/app/app.module.ts
+++ projects/nativescript-lib-demo/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { DemoLibModule } from 'projects/company/demo-lib/src/public-api';
+import { DemoLibModule } from '@company/demo-lib';
 
 @NgModule({
   declarations: [AppComponent],
